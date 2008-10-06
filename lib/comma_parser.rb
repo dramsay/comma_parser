892b1a7428d113a8b261f69e1181c0c1fbbc5226
Doug Ramsay <doug@intridea.com>
2008-10-06 16:42:35 -0400

    Doing this in a simpler and better way

--- lib/comma_parser.rb
+++ lib/comma_parser.rb
@@ -2,21 +2,15 @@ module CommaParser
   
   module ParserMethods
     def allow_commas(*args)
-      before_validation :strip_commas
-      
-      type_cast_array = []
       args.each do |arg|
-        type_cast_array << "#{arg}_will_change!" 
-        type_cast_array << "#{arg}_before_type_cast.to_s.tr!(',', '').to_i"
-      end
-      
-      class_eval(
-        %Q(
-          def strip_commas
-            #{type_cast_array.join(";")}
-          end
+        class_eval(
+          %Q(
+            def #{arg}=(val)
+              super(val.to_s.tr(',',''))
+            end
+          )
         )
-      )
+      end
     end
   end
 end
